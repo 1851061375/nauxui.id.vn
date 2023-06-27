cc3766700a2f7af03c17f5860e819fc72001a324
Lập Phùng <lappv@tandan.com.vn>
2023-06-27 08:36:19 +0700

    change port

--- nauxui/src/index.js
+++ nauxui/src/index.js
@@ -4,7 +4,7 @@ const handlebars = require('express-handlebars');
 const methodOverride = require('method-override')
 const path = require('path');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 const route = require('./routes');
 const db = require('./config/db')
