56903722ec9a78b5c3c8d6a5b21b3a55a34587f0
Lập Phùng <lappv@tandan.com.vn>
2023-06-27 09:14:49 +0700

    1

--- nauxui/src/routes/pill.js
+++ nauxui/src/routes/pill.js
@@ -9,6 +9,7 @@ router.get('/edit/:id', pillController.edit);
 router.put('/:id', pillController.update);
 router.post('/store', pillController.store);
 router.get('/create', pillController.create);
+router.get('/view', pillController.indexView);
 router.get('/', pillController.index);
 
 module.exports = router;
